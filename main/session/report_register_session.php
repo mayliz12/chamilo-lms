32368f0e2dafc720dcbadf0d70125ae9e1934cc6
Alex Aragón <aragcar@gmail.com>
2020-03-05 11:49:25 -0500

    add table report

--- main/session/report_register_session.php
+++ main/session/report_register_session.php
@@ -26,15 +26,39 @@ $em = Database::getManager();
 $sessionCategoryRepository = $em->getRepository('ChamiloCoreBundle:SessionCategory');
 $sessionCategory = $sessionCategoryRepository->findAll();
 
-var_dump($sessionCategory);
+
+$listCategory = [];
+
+foreach ($sessionCategory as $category){
+    $listCategory[$category->getId()] = $category->getName();
+}
 
 $sessionRepository = $em->getRepository('ChamiloCoreBundle:Session');
 $sessions = $sessionRepository->findAll();
 
+$form = new FormValidator('session_report', 'post', api_get_self(),[],[]);
+$form->addSelect('category_session', get_lang('SessionsCategories'), $listCategory);
+$form->addDatePicker('date_start', get_lang('DateStart'), false);
+$form->addDatePicker('date_end', get_lang('DateEnd'), false);
+$content = $form->returnForm();
+
+$listSession = [];
 foreach ($sessions as $session){
 
+    $listSession[] = [
+        'id' => $session->getId(),
+        'name' => $session->getName(),
+        'duration' => $session->getDuration(),
+        'number_users' => $session->getnbrUsers(),
+        'number_courses' => $session->getnbrCourses(),
+        'display_start_date' => $session->getDisplayStartDate(),
+            'display_end_date' => $session->getDisplayEndDate(),
+
+    ];
 }
 
 $tpl = new Template($tool_name);
+$tpl->assign('form_search', $content);
+$tpl->assign('sessions', $listSession);
 $layout = $tpl->get_template('session/report_register_session.tpl');
 $tpl->display($layout);
--- main/template/default/session/report_register_session.tpl
+++ main/template/default/session/report_register_session.tpl
@@ -2,4 +2,43 @@
 
 {% block content %}
 
+    {{ form_search }}
+
+    {{ dump(sessions) }}
+
+
+    <table id="report_session" class="table table-hover">
+        <tr>
+            <th>
+                #
+            </th>
+            <th>
+                {{ 'SessionName' | get_lang }}
+            </th>
+            <th>
+                {{ 'SessionDurationTitle' | get_lang }}
+            </th>
+            <th>
+                {{ 'N° de Usuarios registrados' | get_lang }}
+            </th>
+            <th>
+                {{ 'Número de cursos' | get_lang }}
+            </th>
+        </tr>
+        {% for session in sessions %}
+            <tr>
+                <td>
+                    {{ session.id }}
+                </td>
+                <td>
+                    {{ session.name }}
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        {% endfor %}
+    </table>
+
+
 {% endblock %}
